2fd31f2b054d19199a6a53278ed2d9d67688a291
RogerCastillo877 <roger.castillo877@gmail.com>
2022-07-18 19:20:02 -0500

    Add View Note

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,14 @@
 import { JournalLayout } from '../layout/JournalLayout';
-import { NothingSelectedView } from '../views';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium, autem nemo ducimus temporibus ab optio distinctio tempore vero iste quibusdam est ipsum laudantium iusto quis ea non nesciunt id accusamus!</Typography> */}
 
-      <NothingSelectedView />
+      {/* <NothingSelectedView /> */}
+
+      <NoteView />
     </JournalLayout>
   )
 }
--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,6 +1,41 @@
+import { Button, Grid, TextField, Typography } from "@mui/material"
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
+import { ImageGallery } from "../components";
 
 export const NoteView = () => {
   return (
-    <div>NoteView</div>
+    <Grid container direction='row' justifyContent='space-between' alignItems='center' sx={{ mb: 1 }}>
+      <Grid item>
+        <Typography fontSize={ 39 } fontWeight='light'>28 julio, 2023</Typography>
+      </Grid>
+      <Grid item>
+        <Button color="primary" sx={{ padding: 2 }}>
+          <SaveOutlinedIcon sx={{ fontSize:30, mr: 1 }}/>
+          Guardar
+        </Button>
+      </Grid>
+
+      <Grid container>
+        <TextField
+          type="text"
+          variant="filled"
+          fullWidth
+          placeholder="Ingrese un título"
+          label="Título"
+          sx={{ border: 'none', mb: 1 }}
+        />
+
+        <TextField
+          type="text"
+          variant="filled"
+          fullWidth
+          multiline
+          placeholder="¿Qué sucedió el día de hoy"
+          minRows={ 5 }
+        />
+      </Grid>
+
+      <ImageGallery />
+    </Grid>
   )
 }
