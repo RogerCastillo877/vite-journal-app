78ce9378cb621c5e0944b09c9b3cd4e600e4ec6b
RogerCastillo877 <roger.castillo877@gmail.com>
2022-10-07 09:53:12 -0500

    errors manage

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,11 +1,32 @@
 import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks';
+
+const formData ={
+  email: 'email@gmail.com',
+  password: '123456',
+  displayName: 'Rocas'
+}
+
+const formValidations = {
+  email: [ (value) => value.includes('@'), 'El correo debe tener formato correcto' ],
+  password: [ (value) => value.length >= 6, 'El password deber tener más de 6 caracteres' ],
+  displayName: [ (value) => value.length >= 1, 'El nombre es obligatorio' ],
+}
 
 export const RegisterPage = () => {
+
+  const { displayName, email, password, onInputChange, formState, displayNameValid, emailValid, passwordValid } = useForm( formData, formValidations );
+
+  const onSubmit = ( event ) => {
+    event.preventDefault();
+    console.log(formState)
+  }
+
   return (
     <AuthLayout title="Register">
-      <form>
+      <form onSubmit={ onSubmit }>
         <Grid container>
           <Grid item xs={ 12 } sx={{ mt: 2 }}>
             <TextField
@@ -13,6 +34,11 @@ export const RegisterPage = () => {
               type="text"
               placeholder="Peipito Perez"
               fullWidth
+              name='displayName'
+              value={ displayName }
+              onChange={ onInputChange }
+              error={ !displayNameValid }
+              helperText={ displayNameValid }
             />
           </Grid>
 
@@ -22,6 +48,9 @@ export const RegisterPage = () => {
               type="email"
               placeholder="correo@google.com"
               fullWidth
+              name='email'
+              value={ email }
+              onChange={ onInputChange }
             />
           </Grid>
 
@@ -31,13 +60,19 @@ export const RegisterPage = () => {
               type="password"
               placeholder="Contraseña"
               fullWidth
+              name='password'
+              value={ password }
+              onChange={ onInputChange }
             />
           </Grid>
 
           <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
             
             <Grid item xs={ 12 }>
-              <Button variant='contained' fullWidth>
+              <Button
+                type='submit'
+                variant='contained'
+                fullWidth>
                 Crear Cuenta
               </Button>
             </Grid>
