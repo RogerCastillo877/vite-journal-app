23cb0a338a8c22d58278b565d66f4493486fd027
RogerCastillo877 <roger.castillo877@gmail.com>
2022-10-13 12:07:43 -0500

    structure slice

---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const journalSlice = createSlice({
+  name: 'journal',
+  initialState: {
+    isSaving: true
+  },
+  reducers: {
+  },
+})
+
+export const {} = journalSlice.actions;
