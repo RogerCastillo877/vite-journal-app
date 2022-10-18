fa245d3890df8100c1f5c0274ba48e34b419bcd6
RogerCastillo877 <roger.castillo877@gmail.com>
2022-10-18 17:35:09 -0500

    Test to others reducers in authSlice

--- test/store/auth/authSlice.test.js
+++ test/store/auth/authSlice.test.js
@@ -1,4 +1,6 @@
-import { authSlice } from "../../../src/store/auth/authSlice"
+import { authSlice, checkingCredentials, login, logout } from "../../../src/store/auth/authSlice";
+import { authenticatedState, demoUser, initialState, notAuthenticatedState } from "../../fixtures/authFixtures";
+
 
 describe('should test auhtSlice', () => {
   
@@ -8,5 +10,55 @@ describe('should test auhtSlice', () => {
     const state = authSlice.reducer( initialState, {});
 
     expect( state ). toEqual( initialState )
-  })
-})
+  });
+
+  test('should do the authentication', () => {
+
+    const state = authSlice.reducer( initialState, login(demoUser) );
+
+    expect( state ).toEqual({
+      status: 'authenticated',
+      uid: demoUser.uid,
+      email: demoUser.email,
+      displayName: demoUser.displayName,
+      photoURL: demoUser.photoURL,
+      errorMessage: null
+    })
+  });
+
+  test('should do the logout without arguments', () => {
+    
+    const state = authSlice.reducer( authenticatedState, logout());
+
+    expect( state ).toEqual({
+      status: 'not-authenticated',
+      uid: null,
+      email: null,
+      displayName: null,
+      photoURL: null,
+      errorMessage: undefined
+    });
+  });
+
+  test('should do the logout and display error message', () => {
+    
+    const errorMessage = 'Credenciales no son correctas'
+    const state = authSlice.reducer( notAuthenticatedState, logout({ errorMessage }) )
+
+    expect( state ).toEqual({
+      status: 'not-authenticated',
+      uid: null,
+      email: null,
+      displayName: null,
+      photoURL: null,
+      errorMessage: errorMessage
+    });
+  });
+  
+  test('should change state to "checking', () => {
+
+    const state = authSlice.reducer( authenticatedState, checkingCredentials() );
+
+    expect( state.status ).toBe('checking');
+  });
+});
